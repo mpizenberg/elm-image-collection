85e8f5d209946fd80ab435534314ee1614eec891
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2016-11-08 23:15:29 +0800

    Add doc comments in ImageCollection.elm.

--- src/ImageCollection.elm
+++ src/ImageCollection.elm
@@ -1,7 +1,6 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/
---module ImageCollection exposing (..)
 
 
 module ImageCollection exposing (..)
@@ -21,7 +20,7 @@ import Image exposing (Image)
 -- MODEL #############################################################
 
 
-{-| The key type to access images in the collection
+{-| The key type to access images in the collection.
 -}
 type alias Key =
     String
@@ -51,6 +50,33 @@ defaultItemViewer attributes maybeSize key image =
 
 
 {-| View a collection of images with a specific item viewer.
+
+For example, with the following code:
+
+    collection =
+        Dict.fromList
+            [ ("1", Image "1.jpg" 320 240)
+            , ("2", Image "2.jpg" 640 480)
+            ]
+
+    html =
+        view
+            defaultItemViewer
+            [ HA.class "image" ]
+            Nothing
+            [ HA.class "collection" ]
+            collection
+
+The html will look like:
+
+    <div class="collection">
+        <img class="image"
+            src="1.jpg"
+            style="width: auto; height: auto; max-width: 320px; max-height: 240px;">
+        <img class="image"
+            src="2.jpg"
+            style="width: auto; height: auto; max-width: 640px; max-height: 480px;">
+    </div>
 -}
 view :
     ItemViewer msg
